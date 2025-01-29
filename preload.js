857057f4f5e48a51cf5c88acbb584d78ac079ed0
allenw <allenyu172@gmail.com>
2025-01-29 12:53:41 +0800

    add upload icon and open-file (upload-file not implemented yet)

--- preload.js
+++ preload.js
@@ -2,5 +2,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electron', {
   sendMessage: (message) => ipcRenderer.send('send-message', message),
-  onReceiveMessage: (callback) => ipcRenderer.on('receive-message', (event, message) => callback(message))
+  onReceiveMessage: (callback) => ipcRenderer.on('receive-message', (event, message) => callback(message)),
+  openDirectoryDialog: () => ipcRenderer.invoke('open-directory-dialog')
 });
