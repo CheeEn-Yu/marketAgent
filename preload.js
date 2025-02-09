b071621c86e21f27022794c676ef8739adc09c02
allenw <allenyu172@gmail.com>
2025-02-09 23:15:23 +0800

    vertex AI ver 0

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  sendMessage: (message) => ipcRenderer.send('send-message', message),
-  onReceiveMessage: (callback) => ipcRenderer.on('receive-message', (event, message) => callback(message)),
-  openDirectoryDialog: () => ipcRenderer.invoke('open-directory-dialog')
-});
